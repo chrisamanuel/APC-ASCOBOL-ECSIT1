d97e910a9c811222908e71ce24678b0124dea372
Christian Andrew Manuel <manuel.chrisa@gmail.com>
2017-02-28 10:29:38 +0800

    Added needed files for Midterms

---
+++ 01 Basic Cobol Programs/Shortest.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "I did it".
+           STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
